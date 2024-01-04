340366ba4b22254454ca7a0e5d33837f1ffd322d
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2024-01-04 12:26:39 -0300

    Del: pastas de calculadora

--- Calculator - 4/main.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('form-calculadora')
-
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-
-    const nomeAtividade = document.getElementById('nome-atividade')
-    const notaAtividade = document.getElementById('')
-})
